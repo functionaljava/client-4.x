c62c83730bfb5a744156926292f13f96905baf6d
Mark Perry <maperry78+github@gmail.com>
2015-09-04 16:54:44 +1000

    Added main class and test

---
+++ src/test/java/fj/consume/euler/TestProblem1.java
@@ -0,0 +1,14 @@
+package fj.consume.euler;
+
+import org.junit.Test;
+
+/**
+ * Created by maperry on 4/09/2015.
+ */
+public class TestProblem1 {
+
+    @Test
+    public void problem1() {
+        Problem1.calc();
+    }
+}
